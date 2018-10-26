c8b4595e4578245b8f5c4aaeebd34f412b8d923f
Takahiro SHIMIZU <anatofuz@gmail.com>
2018-10-26 09:57:12 +0900

    minil new and define ChatBot.pm

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Pro2::ChatBot - It's new $module
+
+# SYNOPSIS
+
+    use Pro2::ChatBot;
+
+# DESCRIPTION
+
+Pro2::ChatBot is ...
+
+# LICENSE
+
+Copyright (C) Takahiro SHIMIZU.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Takahiro SHIMIZU <anatofuz@gmail.com>
---
+++ lib/Pro2/ChatBot.pm
@@ -0,0 +1,83 @@
+package Pro2::ChatBot;
+use 5.008001;
+use strict;
+use warnings;
+use Pro2::ReplyPattern;
+
+our $VERSION = "0.01";
+
+sub new {
+    my ($class,%args) = @_;
+
+    my $self = {};
+    $self->{botname} = $args{botname} or die 'please input botname';
+    my @patterns = ();
+    $args{dataset} || die 'unko';
+
+    for my $data (@{$args{dataset}}){
+            my($pattern,$reply) = split /"\t"/,$data;
+            push @patterns, Pro2::ReplyPattern->new(pattern => $pattern,reply => $reply);
+        }
+
+    $self->{patterns} = \@patterns or die 'please input patterns';
+
+    return bless $self,$class;
+}
+
+sub searchPattern {
+    my ($self,$pattern) = @_;
+
+    for my $i (0..@{$self->{patterns}}){
+        if ($pattern eq $self->{patterns}[$i]){
+            return $i;
+        }
+    }
+
+    map { if ("other" eq $self->{patterns}[$_]){ retrun $i;}} (0..@{$self->{patterns}});
+
+    return -1;
+}
+
+sub greeting {
+    my $self = shift @_;
+    my $index = $self->searchPattern("greeting");
+    print ("$self->{botname} 「 $self->{patterns}->[$index]->{reply} 」\n");
+}
+
+sub reply {
+    my ($self,$pattern) = @_;
+    my $index = $self->searchPattern($pattern);
+    my $message = "$self->{botname} 「 $self->{patterns}->[$index]->{reply} 」\n";
+    return $message;
+}
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Pro2::ChatBot - It's new $module
+
+=head1 SYNOPSIS
+
+    use Pro2::ChatBot;
+
+=head1 DESCRIPTION
+
+Pro2::ChatBot is ...
+
+=head1 LICENSE
+
+Copyright (C) Takahiro SHIMIZU.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Takahiro SHIMIZU E<lt>anatofuz@gmail.comE<gt>
+
+=cut
+
